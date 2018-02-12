3cf4cd042cc686792bd10a3974715a066e51615c
jzhangcw <jzhangcw@csl4wk65.win2k.cs.ust.hk>
2018-02-12 16:31:22 +0800

    Add a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -1,5 +1,8 @@
 package labs.comp3111.ust.hk;
 
 public class Lab1 {
-
+	public static void main(String[] argc) {
+	System.out.println("Lab1: Introduction to Git and Github");
+	System.out.println("Congratulation! You learned the Git Basics");
+	}
 }
